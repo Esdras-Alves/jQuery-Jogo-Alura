6854c8a23bf9f783d7e99e5a57d0634920674bb3
Esdras-Alves <esdrasbalves10@gmail.com>
2023-10-10 22:44:30 -0300

    commit

--- servidor/config/express.js
+++ servidor/config/express.js
@@ -3,7 +3,7 @@ var consign = require('consign');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.use(express.static('./../public'));
+app.use(express.static('../../'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
